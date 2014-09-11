0f6d171f3fd41f54bf972f107f55de9e56a4effd
todesking <discommunicative@gmail.com>
2014-09-11 16:12:42 +0900

    query

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,17 @@ package com.todesking.jcon
 import org.rogach.scallop.ScallopConf
 import java.sql.Connection
 
-object Imports extends scalaz.syntax.ToIdOps
+object Imports extends scalaz.syntax.ToIdOps {
+  private type HasClose = {def close():Unit}
+  class Using[A <: HasClose](resource:A) {
+    def apply[B](f:A => B):B =
+      try { f(resource) } finally { resource.close() }
+    def foreach(f:A => Unit) = apply(f)
+  }
+
+  def using[A <: HasClose](resource:A):Using[A] =
+    new Using(resource)
+}
 import Imports._
 
 class Args(raw:Array[String]) extends ScallopConf(raw) {
@@ -14,10 +24,6 @@ class Args(raw:Array[String]) extends ScallopConf(raw) {
 
 
 object Main {
-
-  def using[A <: {def close():Unit}, B](resource:A)(f:A => B):B = {
-    try { f(resource) } finally { resource.close() }
-  }
 
   def main(raw:Array[String]):Unit = {
     val args = new Args(raw)
@@ -57,11 +63,29 @@ case class Command(proc:Context=>Boolean) {
 class Context(val con:Connection, val out:Out)
 
 class Out(val out:java.io.PrintStream) {
+  def error(e:Throwable):Unit = {
+    error(e.toString)
+    e.getStackTrace.foreach{st => error(st.toString)}
+  }
+  def error(e:java.sql.SQLException):Unit = {
+    error(e.getMessage)
+  }
   def error(message:String):Unit = {
     out.println(s"ERROR: ${message}")
   }
   def result(message:String):Unit = {
     out.println(s"${message}")
+  }
+  def result(res:java.sql.ResultSet):Unit = {
+    val meta = res.getMetaData
+    val cols = for(i1 <- 1 to meta.getColumnCount) yield (i1, meta.getColumnName(i1), meta.getColumnType(i1))
+    val rows = scala.collection.mutable.ArrayBuffer.empty[Seq[String]]
+    while(res.next()) {
+      rows += (for { (i1, _, _) <- cols } yield res.getObject(i1).toString)
+    }
+    val widths = cols.map{case (i1, name, _) => Seq(name.size, rows.maxBy{row => row(i1 - 1).size}.size).max}
+    result(cols.map{case (i1, name, _) => s"%-${widths(i1 - 1)}s".format(name)}.mkString(" | "))
+    rows.foreach{row => result(row.zipWithIndex.map{case (r, i) => s"%-${widths(i)}s".format(r)}.mkString(" | "))}
   }
 }
 
@@ -69,7 +93,7 @@ object Command {
   import scala.util.parsing.combinator._
 
   object Parser extends RegexParsers {
-    val ALL = ("""exit|quit|:?q""".r ^^ {q => Quit}) | (":info" ^^ {_ => Info })
+    val ALL = ("""exit|quit|:?q""".r ^^ {q => Quit}) | (":info" ^^ {_ => Info }) | ("[^:].*".r ^^ {q => Query(q)})
     def parse(content:String) = parseAll(ALL, content)
   }
 
@@ -80,8 +104,25 @@ object Command {
   def SyntaxError(line:String) = Command {ctx => ctx.out.error(s"Syntax error: ${line}"); false}
   val Quit = Command {ctx => ctx.out.result("BYE"); true}
   val Info = Command {ctx =>
+    val meta = ctx.con.getMetaData
+    ctx.out.result(s"DRIVER: ${meta.getDriverName} ${meta.getDriverVersion} ${ctx.con.getClass.getName}")
+    ctx.out.result(s"METADATA: ${meta}")
     ctx.out.result(s"CLIENT INFO: ${ctx.con.getClientInfo}")
-    ctx.out.result(s"METADATA: ${ctx.con.getMetaData}")
+    false
+  }
+  def Query(q:String) = Command {ctx =>
+    val con = ctx.con
+    try {
+      for {
+        st <- using(con.createStatement())
+        result <- using(st.executeQuery(q))
+      } {
+        ctx.out.result(result)
+      }
+    } catch {
+      case e:java.sql.SQLException => ctx.out.error(e)
+      case e:Exception => ctx.out.error(e)
+    }
     false
   }
 }
