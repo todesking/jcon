a6ff61c77bebefd1295aed127c9b00799ced1398
todesking <discommunicative@gmail.com>
2014-09-14 19:12:42 +0900

    deal with SIGCONT

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,6 +12,15 @@ object Main {
     val url = trailArg[String]()
   }
 
+  def registerSignal(sig:String)(handler:() =>Unit):Unit = {
+    import sun.misc.{Signal, SignalHandler}
+    Signal.handle(new Signal(sig), new SignalHandler() {
+      override def handle(signal:Signal):Unit = {
+        handler()
+      }
+    })
+  }
+
   def main(raw:Array[String]):Unit = {
     val args = new Args(raw)
     if(!args.url.supplied) {
@@ -28,6 +37,7 @@ object Main {
         con <- using(java.sql.DriverManager.getConnection(url, props))
         terminal <- using[scala.tools.jline.Terminal](scala.tools.jline.TerminalFactory.create(), _.restore())
       } {
+        registerSignal("CONT"){()=> terminal.reset() }
         terminal.init()
         val ctx = new Context(con, new Out(System.out, terminal), createReader(terminal))
         runREPL(ctx)
