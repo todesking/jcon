3ca4475cbe36161e5c49c154eb545c15c321587e
todesking <discommunicative@gmail.com>
2014-09-14 15:37:04 +0900

    refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,16 +17,12 @@ object Imports extends scalaz.syntax.ToIdOps {
     new Using(resource, close)
 
   implicit class Transpose[A](self:Seq[Seq[A]]) {
-    def transposeWithDefault(default:A):Seq[Seq[A]] = {
-      val h = self.size
-      val w = self.map(_.size).max
-      for {
-        c <- 0 until w
-      } yield for {
-        r <- 0 until h
-      } yield {
-        val row = self(r)
-        if(c < row.size) row(c) else default
+    def quadrilateralize(default:A):Seq[Seq[A]] = {
+      val w = self.toIterator.map(_.size).max
+      self.map {row =>
+        for(c <- 0 until w) yield {
+          if(c < row.size) row(c) else default
+        }
       }
     }
   }
@@ -157,9 +153,9 @@ class Out(val out:java.io.PrintStream, val terminal:scala.tools.jline.Terminal) 
     // if(widths.sum + (widths.size - 1) * 3/*sep*/ + 4/*start+end*/ > displayWidth)
     def rowsep() = result("+" + cols.map{case (i1, name) => "-" * (widths(i1 - 1) + 2)}.mkString("+") + "+")
     def outRow(row:Seq[String]) = {
-      val cols:Seq[Seq[String]] = row.map(_.split("\n").toSeq).transposeWithDefault("")
-      val maxHeight = cols.map(_.size).max
-      cols.transposeWithDefault("").transpose.zipWithIndex.foreach {case (row, i) =>
+      val subRows:Seq[Seq[String]] = row.map(_.split("\n").toSeq).quadrilateralize("").transpose
+      val maxHeight = subRows.map(_.size).max
+      subRows.zipWithIndex.foreach {case (row, i) =>
         val sep = if(i == 0) "|" else ">"
         result(s"$sep " + row.zipWithIndex.map{case (r, i) => r.pad(widths(i), ' ', "...")}.mkString(s" $sep ") + " |")
       }
